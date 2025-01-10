c08d9f456757ece77652f65c0953009538572d72
royerm <royerm@gmail.com>
2025-01-10 16:00:20 +0100

    Remove VitePWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@
 
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate' }),
     legacy()
   ],
   build: {
