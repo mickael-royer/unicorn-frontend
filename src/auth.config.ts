7504138efedfd6ba2742c14d1554844fdff03208
royerm <royerm@gmail.com>
2025-01-10 15:38:57 +0100

    Update Auth0 config

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,7 +1,7 @@
 // import { isPlatform } from "@ionic/react";
 
 export const domain = import.meta.env.VITE_AUTH0_DOMAIN || 'royerm.eu.auth0.com';
-export const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID || '';
+export const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID || 'bVa2PDrmp8zmJ9tumzIrDa1lmgQ9IkxX';
 
 // Use `auth0Domain` in string interpolation below so that it doesn't
 // get replaced by the quickstart auto-packager
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@
 
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({ registerType: 'autoUpdate' }),
     legacy()
   ],
   build: {
