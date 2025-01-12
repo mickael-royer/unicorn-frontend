59a1f500bd02153c7f3143ef8afa36948a0db4f5
royerm <royerm@gmail.com>
2025-01-12 20:41:04 +0100

    Update Notification ID

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,2 +1,2 @@
 export const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'https://api.royerm.fr';
-export const notificationId = import.meta.env.VITE_PUSHER_CLIENT_ID || '';
+export const notificationId = import.meta.env.VITE_PUSHER_CLIENT_ID || '404b62fffcad0de861ed';
